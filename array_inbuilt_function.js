9c48214b126adc062eb0e9978aebea883eaccf69
Intern20 <Intern20@swifttech.com>
2023-01-05 15:12:33 +0545

    pop inbuilt function

---
+++ array_inbuilt_function.js
@@ -0,0 +1,17 @@
+const func= ()=>
+{ 
+
+    // push inbuilt function
+    const arr = [1,2,3,4]
+    arr.push(6);
+    console.log(arr);
+
+    // pop inbuilt function
+    const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+    plants.pop();
+     console.log(plants);
+
+    
+}
+let arry= func();
+console.log(arry);
