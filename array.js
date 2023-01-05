d23808bad08df9efd27e4cc05966a3f49760acc0
Intern20 <Intern20@swifttech.com>
2023-01-05 14:58:38 +0545

    array is initialized

---
+++ array.js
@@ -0,0 +1,10 @@
+const arr= ()=>
+{
+
+    // this is my first array function 
+    let myArr = ["Banana","Apple","orange"];
+    console.log(`i love to eat ${myArr[2]}`);
+}
+
+let temp= arr();
+console.log(temp);
