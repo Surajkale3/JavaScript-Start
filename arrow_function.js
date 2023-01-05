060cf5d5f10af63d270a25cf52ae784e378147d4
Intern20 <Intern20@swifttech.com>
2023-01-05 14:47:41 +0545

    init of my first repo

---
+++ arrow_function.js
@@ -0,0 +1,18 @@
+
+
+const arrfun= ()=>
+{
+    let obj = 
+{
+    name:"Suraj",
+    surName:" Sapkota",
+    age:22
+}
+ console.log(obj.name + obj.surName);
+    
+}
+
+
+
+let varrr=arrfun();
+console.log(varrr);
---
+++ template_literal.js
@@ -0,0 +1,17 @@
+const litral =()=>
+{
+    let refliteral= 
+    {
+        organizationName:"midas Technology",
+        Location:"Panipokhari Ime Complex",
+        PostOfficeCode:255
+    }
+
+console.log(`Suraj shapkota is working on office name ${refliteral.organizationName}, The location of 
+suraj office is ${refliteral.Location}  He use to send letter to his office ${refliteral.organizationName} and 
+he uses post code number ${refliteral.PostOfficeCode} `);
+
+}
+
+let func= litral()
+console.log(func);
