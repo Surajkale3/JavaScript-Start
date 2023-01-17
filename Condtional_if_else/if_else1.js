4c02cdc365dd11c5222bef17fc5d03473baf4761
Intern20 <Intern20@swifttech.com>
2023-01-17 10:10:39 +0545

    if else statement

---
+++ Condtional_if_else/if_else1.js
@@ -0,0 +1,11 @@
+const numberCheck= (num)=>
+{
+    if (num>0)
+    {
+        console.log("The number is positive");
+    }
+    else{
+        console.log("The number is negative");
+    }
+}
+console.log(numberCheck(-3));
